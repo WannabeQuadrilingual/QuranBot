1f7cf6e617922a1e6c66cf11a7ac307934f4c852
gibreel <wannabequadrilingual@gmail.com>
2018-12-22 12:14:41 +0530

    Multiple Ayah support

--- quranbot.py
+++ quranbot.py
@@ -14,18 +14,35 @@ async def on_message(message):
 	# we do not want the bot to reply to itself
 	if message.author == client.user:
 		return
-	searchObj = re.search( r'qur\'?an \b([1][0,1][0,1,2,3,4]|[1-9][0-9]?)\b:([0-9]{1,3})\b', message.content.lower(), re.I)
+	searchObj = re.search( r'qur\'?an \b([1][0,1][0,1,2,3,4]|[1-9][0-9]?)\b:([0-9]{1,3})\b-?(\b([0-9]{1,3})\b)?', message.content.lower(), re.I)
 	if (searchObj):
-		print (searchObj.group(0))
-		print (searchObj.group(1))
-		print (searchObj.group(2))
+		if(searchObj.group(3)):
+			print (searchObj.group(0))
+			print (searchObj.group(1))
+			
+			start = int(searchObj.group(2))
+			end = int(searchObj.group(3))
 
-		msg = requests.get(url = f'http://api.alquran.cloud/ayah/{searchObj.group(1)}:{searchObj.group(2)}/en.asad')
-		response_text = json.loads(msg.text)
-		ayah_ar = "Qur'an " + searchObj.group(1) + ":" + searchObj.group(2) + "\n" + response_text['data']['text']
-		print(ayah_ar)
+			print (start)
+			print (end)
 
-		await client.send_message(message.channel, ayah_ar)
+			for x in range (start,end+1):
+
+				msg = requests.get(url = f'http://api.alquran.cloud/ayah/{searchObj.group(1)}:{str(x)}/en.yusufali')
+				response_text = json.loads(msg.text)
+				ayah_ar = "Qur'an " + searchObj.group(1) + ":" + str(x) + "\n" + response_text['data']['text']
+				print(ayah_ar)
+				await client.send_message(message.channel, ayah_ar)
+
+		else:
+			print (searchObj.group(0))
+			print (searchObj.group(1))
+			print (searchObj.group(2))
+			msg = requests.get(url = f'http://api.alquran.cloud/ayah/{searchObj.group(1)}:{searchObj.group(2)}/en.yusufali')
+			response_text = json.loads(msg.text)
+			ayah_ar = "Qur'an " + searchObj.group(1) + ":" + searchObj.group(2) + "\n" + response_text['data']['text']
+			print(ayah_ar)
+			await client.send_message(message.channel, ayah_ar)
 
 @client.event
 async def on_ready():
